3acb999a848867c0e42cad1667c5f6336cdbc344
Josiah-low <e1397806@u.nus.edu>
2026-03-15 21:30:39 +0800

    Add support for telegram handle in PersonCard component

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -35,7 +35,7 @@ public class PersonUtil {
         sb.append(PREFIX_PHONE + person.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
         sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
-        sb.append(PREFIX_TELEGRAM + person.getTelegram().telegramHandle + " ");
+        sb.append(PREFIX_TELEGRAM + person.getTelegram().value + " ");
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
