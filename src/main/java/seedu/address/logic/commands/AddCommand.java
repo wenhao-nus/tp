6e42ca98a30b3e34ce7cdde3548ec7cbcccf5884
wenhao <111180600+wenhao-nus@users.noreply.github.com>
2026-03-17 17:25:57 +0800

    Refactor to adhere to checkstyle

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -39,8 +39,8 @@ public class AddCommand extends Command {
             + PREFIX_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "Email must be unique!" +
-            "This email already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "Email must be unique!"
+            + "This email already exists in the address book.";
 
     private final Person toAdd;
 
