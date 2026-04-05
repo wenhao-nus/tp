8c7a9c074c4f72c6f3fa4b9cbb6b465694005657
e-hua <eclipsehua@gmail.com>
2026-04-05 23:53:54 +0800

    Change the user guide url in HelpWindow

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -15,7 +15,7 @@ import seedu.address.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
+    public static final String USERGUIDE_URL = "https://ay2526s2-cs2103t-t10-4.github.io/tp/UserGuide.html";
     public static final String HELP_MESSAGE =
             "Add: add n/NAME e/EMAIL [p/PHONE_NUMBER] [a/ADDRESS] [tg/TELEGRAM_HANDLE] [t/TAG]…\n"
             + "Delete: delete INDEX\n"
