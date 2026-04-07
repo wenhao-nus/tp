70edbdc73c78ae4fe6bb2d020c7c74869d025faa
kelv5 <ngkelvinkahkit@gmail.com>
2026-04-07 12:43:09 +0800

    Add a newline for readability

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -52,7 +52,7 @@ public class EditCommand extends Command {
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_PERSON = "Email, Telegram handle, and phone number must be unique!"
+    public static final String MESSAGE_DUPLICATE_PERSON = "Email, Telegram handle, and phone number must be unique!\n"
             + " A contact with the same email, phone number, or Telegram handle exists in the addressbook.";
 
     private final Index index;
